c27e803f225662a92a1071f5f6a5c618a4a67336
FcoLeyva <Leyva_Perez24@unikino.edu.mx>
2024-08-10 12:35:03 -0700

    Add project files.

---
+++ Lista.cs
@@ -0,0 +1,41 @@
+﻿namespace ListaSimplementeLigadas
+{
+    internal class Lista
+    {
+        private Nodo _nodoInicial;
+        public Lista()
+        {
+            _nodoInicial = new Nodo();
+
+        }
+
+        private bool EstaVacia()
+        {
+            if (_nodoInicial.Enlace == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public void Vaciar()
+        {
+            _nodoInicial.Enlace = null;
+        }
+
+        public void Agregar(string valor)
+        {
+            Nodo nodoActual = _nodoInicial;
+            while (nodoActual.Enlace != null)
+            {
+                nodoActual = nodoActual.Enlace;
+            }
+
+            Nodo nodoNuevo = new Nodo(valor);
+            nodoActual.Enlace = nodoNuevo;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ListaSimplementeLigadas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
